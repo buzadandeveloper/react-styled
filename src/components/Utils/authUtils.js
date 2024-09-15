db4b449137f514d4211d6695d340ddb40c8756f0
Buza Dan <buzadandeveloper@gmail.com>
2024-09-15 23:26:20 +0300

    feat: add auth utilities

---
+++ src/components/Utils/authUtils.js
@@ -0,0 +1,25 @@
+export const saveAuthDataLocalStorage = (authData) => {
+  const users = JSON.parse(localStorage.getItem("users")) || [];
+  users.push(authData);
+  localStorage.setItem("users", JSON.stringify(users));
+};
+
+export const isEmailRegistered = (email) => {
+  const users = JSON.parse(localStorage.getItem("users")) || [];
+  return users.some((user) => user.email === email);
+};
+
+export const checkAuthDataLocalStorage = (authData) => {
+  const users = JSON.parse(localStorage.getItem("users")) || [];
+  const user = users.find(
+    (user) =>
+      user.email === authData.email && user.password === authData.password
+  );
+
+  if (!user)
+    return { success: false, message: "Email or password is incorrect." };
+
+  const loggedInUser = { ...user, isAuthenticated: true };
+  localStorage.setItem("loggedInUser", JSON.stringify(loggedInUser));
+  return { success: true, user: loggedInUser };
+};
