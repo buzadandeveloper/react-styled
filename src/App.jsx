96588c0b9734d8a1edeeb817964a33b674f9cb12
Buza Dan <buzadandeveloper@gmail.com>
2024-09-15 23:39:35 +0300

    fix: update routing logic in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,16 @@
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 import { ThemeProvider } from "styled-components";
-import { Home } from "./components/NavBar/Home/Home";
+import { GlobalStyles } from "./GlobalStyles/GlobalStyles";
+import { Home } from "./components/Home/Home";
+import { Auth } from "./components/Auth/Auth";
+import { ProtectedRoute } from "./components/ProtectedRoute/ProtectedRoute";
+import { Dashboard } from "./components/Dashboard/Dashboard";
+
 function App() {
   const theme = {
     colors: {
@@ -10,12 +21,28 @@ function App() {
       l_grey: "#89939E",
       white: "#FFFFFF",
       shade: "#28CB8B",
+      silver: "#F5F7FA",
     },
   };
 
   return (
     <ThemeProvider theme={theme}>
-      <Home />
+      <GlobalStyles />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/auth" element={<Auth />} />
+          <Route
+            path="/dashboard"
+            element={
+              <ProtectedRoute>
+                <Dashboard />
+              </ProtectedRoute>
+            }
+          />
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+      </Router>
     </ThemeProvider>
   );
 }
